85cda88d679e553b359adc423725941d1893ad83
FreeEcho <20364419+FreeEcho1@users.noreply.github.com>
2023-05-21 10:26:54 +0900

    Add files via upload

---
+++ GetPluginInformation/GetPluginInformation/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace GetPluginInformation;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    protected override void OnStartup(
+        StartupEventArgs e
+        )
+    {
+        base.OnStartup(e);
+
+        Processing.WritePluginsInformation();
+
+        Shutdown();
+    }
+}
---
+++ GetPluginInformation/GetPluginInformation/Processing.cs
@@ -0,0 +1,70 @@
+﻿using System.IO;
+using System;
+using Swindom.IPluginSwindom;
+
+namespace GetPluginInformation;
+
+internal class Processing
+{
+    /// <summary>
+    /// ディレクトリ内のファイルパスを取得
+    /// </summary>
+    /// <returns>ディレクトリ内のファイルパス</returns>
+    private static string[] GetFilesPath()
+    {
+        return Directory.GetFiles(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + Path.DirectorySeparatorChar + "Plugins", "*.dll");
+    }
+
+    /// <summary>
+    /// プラグインの情報を書き込む
+    /// </summary>
+    public static void WritePluginsInformation()
+    {
+        try
+        {
+            string? iPluginTypeName = typeof(IPlugin).FullName;       // プラグインインターフェースの型名
+            if (iPluginTypeName == null)
+            {
+                return;
+            }
+            string[] filesPath = GetFilesPath();     // ディレクトリ内のファイルパス
+
+            foreach (string nowFilePath in filesPath)
+            {
+                try
+                {
+                    System.Reflection.Assembly assembly = System.Reflection.Assembly.LoadFrom(nowFilePath);
+                    foreach (Type type in assembly.GetTypes())
+                    {
+                        // プラグインのみ書き込む
+                        if (type.IsClass && type.IsPublic
+                            && type.IsAbstract == false
+                            && type.GetInterface(iPluginTypeName) != null
+                            && string.IsNullOrEmpty(type.FullName) == false)
+                        {
+                            IPlugin? iPlugin = (IPlugin?)assembly.CreateInstance(type.FullName);
+                            if (iPlugin == null)
+                            {
+                                break;
+                            }
+                            string pluginName = iPlugin.PluginName;
+                            if (string.IsNullOrEmpty(pluginName))
+                            {
+                                pluginName = Path.GetFileNameWithoutExtension(nowFilePath);
+                            }
+                            Console.WriteLine(nowFilePath);
+                            Console.WriteLine(pluginName);
+                            break;
+                        }
+                    }
+                }
+                catch
+                {
+                }
+            }
+        }
+        catch
+        {
+        }
+    }
+}
